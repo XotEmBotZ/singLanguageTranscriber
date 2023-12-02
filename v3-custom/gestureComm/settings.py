2f24cdf724ab94af0571d67ec3c2904efd390bac
Surjyadip Sen <XotEmBotZ>
2023-12-02 12:35:11 +0530

    Django project initiation

--- v3-custom/gestureComm/settings.py
+++ v3-custom/gestureComm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j0(n1m@)@s&wxmj*en_c6t3x21b=5&j4jn8cd1smh9y-!3b$be
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    "whitenoise.runserver_nostatic",
+
+    'home',
+    'notification',
+    'modelCreator'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,3 +129,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
