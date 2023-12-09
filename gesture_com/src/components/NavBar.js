66ca240aa78106f2e0f1760cfa66f6f4aa218376
Surjyadip Sen <XotEmBotZ>
2023-12-09 12:07:23 +0530

    Initialized the GUI

---
+++ gesture_com/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '@/styles/components/navBar.module.css'
+import Link from 'next/link'
+
+const NavBar = () => {
+    return (
+        <nav className={styles.navBar}>
+            <ul className={styles.navUl}>
+                <li><Link href='/'>Home</Link></li>
+                <li><Link href='/detect'>Detect</Link></li>
+                <li><Link href='/'>Train</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
